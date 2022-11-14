575612ed6fc7aecaa2f61ce4648659d366c562e8
bruk19 <brukteshome21@gmail.com>
2022-11-14 18:20:39 +0300

    implement clicked doctors

--- src/features/doctor/Doctor.js
+++ src/features/doctor/Doctor.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Link, } from 'react-router-dom';
+import { getDoctors } from '../../app/doctor';
+import { useDispatch} from 'react-redux';
 import './doctor.css';
 
 function Doctor({name, title, bio, photo, id}) {
 
-  
+  const dispatch = useDispatch();
+  const handleDetails = () => {
+    dispatch(getDoctors(Doctor));
+  }
 
   return (
     <>
     <div className='doctor-box'>
       <Link to='/Doctor'>
-        <img className="doctor-image" src={photo} alt={name}  />
+        <img className="doctor-image" src={photo} alt={name}onClick={handleDetails}  />
       </Link>
       <h3 className="doctor-name">{name}</h3>
       <p className="doctor-description">{title}</p>
