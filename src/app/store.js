8aedaba7f118ac7c953e29807ec5e1f3bfbe0938
Gideon <gideons564@gmail.com>
2022-11-17 12:11:41 +0100

    Fetch and display the reservations

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-<<<<<<< HEAD
-import reservationReducer from './reservation/reservationSlice';
-
-export const store = configureStore({
-  reducer: {
-    reservation: reservationReducer,
-=======
 import counterReducer from '../features/counter/counterSlice';
 import doctorsReducer from '../features/doctor/doctorsSlice';
+import reservationReducer from './reservation/reservationSlice';
 import {userReducer}
  from '../features/user/UserSlice';
 import detailReducer from './detail';
@@ -17,7 +11,7 @@ export const store = configureStore({
     counter: counterReducer,
     user: userReducer,
     doctors: doctorsReducer,
-    detail: detailReducer,
->>>>>>> b330afbe86c6b9ff1119b29509d6b6249fa8e580
+     reservation: reservationReducer,
+    detail: detailReducer
   },
 });
--- src/features/navigation/Navigations.js
+++ src/features/navigation/Navigations.js
@@ -11,32 +11,18 @@ function Navigations() {
   return (
     <nav className="navbar navbar-light bg-light w-25">
         <div className="container-fluid p-5">
-<<<<<<< HEAD
-          <a className ="navbar-brand" href="/">Navbar</a>
-        </div>
-        <div className ="container-fluid p-2">
-          <h4><a className ="navbar-brand" href="/">Doctor</a></h4>
-=======
           <a className="navbar-brand" href="/">Navbar</a>
         </div>
         <div className="container-fluid p-2">
           <h4><a className="navbar-brand" href="/doctors">Doctor</a></h4>
->>>>>>> b330afbe86c6b9ff1119b29509d6b6249fa8e580
         </div>
         <div className="container-fluid p-2">
           <h4><a className="navbar-brand" href="/reserve_form">Make Reservation</a></h4>
         </div>
-<<<<<<< HEAD
-        <div className ="container-fluid p-2">
-          <h4><a className ="navbar-brand" href="/reservation">My Reservations</a></h4>
-        </div>
-        <div className ="container-fluid p-2">
-=======
         <div className="container-fluid p-2">
           <h4><a className="navbar-brand" href="/reservation">My Reservations</a></h4>
         </div>
         <div className="container-fluid p-2">
->>>>>>> b330afbe86c6b9ff1119b29509d6b6249fa8e580
           <h4><a className="navbar-brand" href="/add_doctor">Add Doctor</a></h4>
         </div>
         <div className="container-fluid p-2">
